4763f1eec38293f1e92afd75ecbc9bd3d976f0c3
Hsin Wang <hsinwang5@gmail.com>
2022-04-28 02:13:33 -0400

    Add death animations and triggers

--- Scripts/Combat/Health.cs
+++ Scripts/Combat/Health.cs
@@ -8,9 +8,16 @@ namespace RPG.Combat
     {
         [SerializeField] float health = 100f;
 
+        bool isDead = false;
+
         public void TakeDamage(float damage)
         {
             health = Mathf.Max(health - damage, 0); //returns largest of 2 or more values
+            if (health == 0 && !isDead) 
+            {
+                GetComponent<Animator>().SetTrigger("Die");
+                isDead = true;
+            }
             print(health);
         }
     }
